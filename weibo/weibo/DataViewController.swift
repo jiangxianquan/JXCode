e12cb69b6b698e1e611a1710f9e50460f3d8ac0d
123 <a123@123deMacBook-Pro.local>
2017-06-11 23:33:29 +0800

    初始化项目

---
+++ weibo/weibo/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  weibo
+//
+//  Created by 123 on 2017/6/11.
+//  Copyright © 2017年 123. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
